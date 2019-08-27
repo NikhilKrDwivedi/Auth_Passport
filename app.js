13a2441705a21c590b8bfa9a502ef6c23a80cfc0
NikhilKrDwivedi <authornikhildwivedi@gmail.com>
2019-08-27 17:39:29 +0530

    Auth Completed

--- app.js
+++ app.js
@@ -1,35 +1,52 @@
-const express  =  require('express');
-const path = require('path');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
-const expressLayouts  =  require('express-ejs-layouts');
+
 const db = require('./config/db.js');
 const app = express();
-//EJS
-app.use(expressLayouts);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
-//Body-Parser
-app.use(express.urlencoded({extended:false}));
-//express session midleware
-app.use(session({
-  secret: 'does not matter',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: true }//he remove
-}));
 
-// connect flash
+
+// Passport Config
+require('./config/passport')(passport);
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
 app.use(flash());
 
-//Global Variables
-app.use((req,res,next)=>{
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	next();
-})
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
 // Routes
-app.use('/',require('./routes/index'));
-app.use('/users',require('./routes/users'));
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,console.log(`Server is running at port: ${PORT}`));
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express  =  require('express');
 const router =  express.Router();	
 const bcrypt = require('bcryptjs');
+
+const passport = require('passport');
+
+
 //User Model
 const User = require('../models/User')
 //login Page
@@ -63,7 +67,7 @@ if(errors.length>0){
 					//set password to hashed
 					newUser.password=hash;
 					newUser.save().then(user => {
-						req.flash('success_msg','You are now register');
+						req.flash('success_msg','You are now registered and you can login');
 						res.redirect('/users/login');
 					}).catch(err=>console.log(err));
 			}))
@@ -71,5 +75,20 @@ if(errors.length>0){
 		}
 	});
 }
+});
+//Login Handle
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
 });
 module.exports= router;
